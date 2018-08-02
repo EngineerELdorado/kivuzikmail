e5715e3cc6d55d10237fecabbf7dfd9655028cc2
denis.kalenga <engineereldorado@gmail.com>
2018-08-02 15:57:30 +0300

    cross origins

--- src/main/java/com/api/KivuzikMail/controllers/EmailController.java
+++ src/main/java/com/api/KivuzikMail/controllers/EmailController.java
@@ -8,15 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RestController
 @RequestMapping("/emails")
+@CrossOrigin(origins = "*")
 public class EmailController {
 
      @Autowired
