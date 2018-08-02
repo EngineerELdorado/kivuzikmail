b450d800de8105ea32fe571113e94ec6751edf9a
denis.kalenga <engineereldorado@gmail.com>
2018-08-02 23:07:08 +0300

    changed email

---
+++ src/main/java/com/api/KivuzikMail/configs/SpringBatchConfig.java
@@ -0,0 +1,16 @@
+package com.api.KivuzikMail.configs;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringBatchConfig {
+
+//    @Bean
+//    public Job job(JobBuilderFactory jobBuilderFactory){
+//
+//        jobBuilderFactory.get("")
+//    }
+}
