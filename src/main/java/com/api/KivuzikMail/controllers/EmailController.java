f549f34bbe4cc7aead584158052925787041d1aa
denis.kalenga <engineereldorado@gmail.com>
2018-08-04 11:39:28 +0300

    added logging

--- src/main/java/com/api/KivuzikMail/controllers/EmailController.java
+++ src/main/java/com/api/KivuzikMail/controllers/EmailController.java
@@ -4,6 +4,8 @@ import com.api.KivuzikMail.models.EmailMessage;
 import com.api.KivuzikMail.models.KivuzikUser;
 import com.api.KivuzikMail.services.IEmailService;
 import com.api.KivuzikMail.services.IUserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,15 +23,27 @@ public class EmailController {
      IUserService userService;
      @Autowired
      IEmailService emailService;
+     Logger LOG = LogManager.getLogger(EmailController.class);
 
      HttpHeaders httpHeaders = new HttpHeaders();
      @PostMapping("/send")
      public ResponseEntity<?>sendEmail(@RequestBody EmailMessage emailMessage){
+
+         LOG.info("REQUEST ARRIVED");
          Collection<KivuzikUser>kivuzikUsers = userService.getAll();
 
          for(KivuzikUser kivuzikUser: kivuzikUsers){
              emailMessage.setTo(kivuzikUser.getEmail());
-             emailService.sendSimpleMail(emailMessage);
+             try {
+
+                 emailService.sendSimpleMail(emailMessage);
+                 LOG.info("email envoyE A "+ kivuzikUser.getUsername() +" / "+kivuzikUser.getEmail());
+             }
+             catch (Exception e)
+             {
+                 e.printStackTrace();
+                 LOG.error("error "+e.toString());
+             }
          }
          httpHeaders.add("response_message","Operation reussie");
          return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
